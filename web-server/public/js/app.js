d6d3242ab28057ad21cc461426142ade7479a16e
Akash <iamakashkataria18@gmail.com>
2023-05-06 12:46:25 +0530

    fetching forecast data and modifying text content to display

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,28 @@
 console.log("Client side js is loaded")
+
+const weatherForm = document.querySelector("form")
+const searchElement = document.querySelector("input")
+const locationElement = document.querySelector("#location")
+const forecastElement = document.querySelector("#forecast")
+
+weatherForm.addEventListener("submit", (e)=>{
+    locationElement.textContent = "...loading"
+    forecastElement.textContent = ""
+
+    e.preventDefault()
+    const address = searchElement.value
+
+    fetch(`http://localhost:3000/weather?address=${address}`)
+    .then(response=>{
+        response.json().then((data)=>{
+            if(data.error){
+                locationElement.textContent = data.error
+            }
+            else{
+                const {location, forecast} = data
+                locationElement.textContent = location
+                forecastElement.textContent = "Outlook: " + forecast.weather + ", Temperature: " + forecast.temperature + ", Feels like Temperature: " + forecast.feel 
+            }
+        })
+    })
+})
